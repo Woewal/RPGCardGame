fca6581a2895ae5e5817570bc11de5490813d4df
Dennis Woering <d.woering@hotmail.com>
2019-09-17 00:09:14 +0200

    Added nodes

---
+++ Assets/Scripts/Spell/TargetType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public abstract class TargetType : Node {
+
+	[Output] public List<Character> Targets;
+
+}
